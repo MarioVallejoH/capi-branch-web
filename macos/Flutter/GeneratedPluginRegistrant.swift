47a2f8d2c04286cd0f8abb36cbf62331b3cf89bc
MarioVallejoH <lui_gi06@hotmail.com>
2024-06-17 01:47:04 -0500

    feat: :art: Setting up project structure
    Created folders and files following hexagonal architecture?

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import nb_utils
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NbUtilsPlugin.register(with: registry.registrar(forPlugin: "NbUtilsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <nb_utils/nb_utils_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  NbUtilsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NbUtilsPlugin"));
+}
